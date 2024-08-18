33f05de8d5b2a128dd0b78cc6c49f67299944cdb
YashedP <yashjani144@gmail.com>
2024-08-18 01:26:32 -0400

    6

--- api/vector_search.py
+++ api/vector_search.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/api/vector_search', methods=['GET'])
 def vector_search():
     # Get the 'text' parameter from the request
-    text = request.args.get('text')
+    text = request.args.get('prompt')
 
     # Check if the 'text' parameter is provided
     if text:
