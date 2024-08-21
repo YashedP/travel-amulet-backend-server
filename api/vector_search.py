bc12d574d5955bd7ed4648ea588a50aa266b6b3c
YashedP <yashjani144@gmail.com>
2024-08-20 22:50:28 -0400

    debug 11

--- api/vector_search.py
+++ api/vector_search.py
@@ -25,16 +25,16 @@ def vector_search():
     # Check if the 'text' parameter is provided
     if prompt is not None and crime_index is not None and download_speed and mobile_download_speed is not None is not None and tap_water_index is not None and continent_list is not None and blacklist_countries is not None:
         country = get_countries(prompt, crime_index, download_speed, mobile_download_speed, tap_water_index, continent_list, blacklist_countries)
-        return jsonify({"countries": country, 
+        return jsonify({"countries": country}), 200
+    else:
+        return jsonify({"error": "No string provided",
                         "prompt": prompt,
                         "crime_index": crime_index,
                         "download_speed": download_speed,
                         "mobile_download_speed": mobile_download_speed,
                         "tap_water_index": tap_water_index,
                         "continent_list": continent_list,
-                        "blacklist_countries": blacklist_countries}), 200
-    else:
-        return jsonify({"error": "No string provided", "continents_list": continent_list, "blacklist_countries": blacklist_countries}), 400
+                        "blacklist_countries": blacklist_countries}), 400
     
 def get_countries(prompt, crime_index, download_speed, mobile_download_speed, tap_water_index, continent_list, blacklist_countries):
     tidb_connection_string = os.environ["TIDB_CONNECTION_STRING"]
