2b108f78ff08cf790d5d263bad1d110f7c33c1f7
YashedP <yashjani144@gmail.com>
2024-08-20 21:37:58 -0400

    Added Continents and Mobile Download Speed and Country as filters

--- api/vector_search.py
+++ api/vector_search.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import json
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import TiDBVectorStore
 from langchain_openai import OpenAIEmbeddings
@@ -16,17 +17,19 @@ def vector_search():
     prompt = request.args.get('prompt')
     crime_index = request.args.get('crime_index', type=float)
     download_speed = request.args.get('download_speed', type=int)
+    mobile_download_speed = request.args('mobile_download_speed', type=int)
     tap_water_index = request.args.get('tap_water_index', type=float)
-    
+    continent_list = json.loads(request.args.get('continent_list', type=str))
+    blacklist_countries = json.loads(request.args.get('blacklist_countries', type=str))
 
     # Check if the 'text' parameter is provided
-    if prompt is not None and crime_index is not None and download_speed is not None and tap_water_index is not None:
-        country = get_countries(prompt, crime_index, download_speed, tap_water_index)
+    if prompt is not None and crime_index is not None and download_speed and mobile_download_speed is not None is not None and tap_water_index is not None and continent_list is not None and blacklist_countries is not None:
+        country = get_countries(prompt, crime_index, download_speed, mobile_download_speed, tap_water_index, continent_list, blacklist_countries)
         return jsonify({"countries": country}), 200
     else:
         return jsonify({"error": "No string provided"}), 400
     
-def get_countries(prompt, crime_index, download_speed, tap_water_index):
+def get_countries(prompt, crime_index, download_speed, mobile_download_speed, tap_water_index, continent_list, blacklist_countries):
     tidb_connection_string = os.environ["TIDB_CONNECTION_STRING"]
 
     embeddings = OpenAIEmbeddings()
@@ -38,18 +41,15 @@ def get_countries(prompt, crime_index, download_speed, tap_water_index):
         table_name="vectors",
         distance_strategy="cosine",
     )
-
-    # Finds the most similar document to the query
-    # filters = {
-    #     "Crime_Index":{"$lt": 4.5},
-    #     "Download_Speed":{"$gt": 100},
-    #     "Tap_Water_Index":{"$gt": 80},
-    # }
     
+    # Applies the filters to the query
     filters = {
         "Crime_Index": {"$lt": crime_index},
         "Download_Speed": {"$gt": download_speed},
+        "Mobile_Download_Speed": {"$gt": mobile_download_speed},
         "Tap_Water_Index": {"$gt": tap_water_index},
+        "Continent": {"$nin": continent_list},
+        "Country": {"$nin": blacklist_countries}
     }
 
     docs_with_score = vector_store.similarity_search_with_relevance_scores(prompt, filter=filters, k=10)
